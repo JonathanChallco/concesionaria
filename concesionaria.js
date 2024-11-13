02913151bec74e4694b501828c82fc312acfaf9e
JonathanChallco <jonathanchallco21@gmail.com>
2024-11-12 21:04:16 -0300

    Agregado de funciones

--- concesionaria.js
+++ concesionaria.js
@@ -1,20 +1,29 @@
 const autos = require('./autos.js');
+
 const concesionaria = {
-    vehiculos:autos,
+    vehiculos: autos,
+    listaDeVentas: function () {
+        return this.vehiculos.filter(car => car.vendido === true)
+    },
     autosEnventa: function () {
         return this.vehiculos.filter(car => car.vendido === false)
     },
-    vender:function (dominio) {
+    vender: function (dominio) {
         this.vehiculos.forEach(car => {
-            if(car.patente == dominio){
+            if (car.patente == dominio) {
                 car.vendido = true
             }
         })
     },
-    autoFinanciable:function (cuotas) {
+    autoFinanciable: function (cuotas) {
         const enVenta = this.autosEnventa();
         return enVenta.filter(car => car.cuotas >= cuotas);
+    },
+    totalDeVentas: function () {
+        return this.listaDeVentas().reduce((total, vehiculo) => total + vehiculo.precio, 0);
+    },
+    autosQuePuedeComprar: function(importe) {
+        return this.vehiculos.filter(car => car.precio <= importe && car.vendido === false);
     }
 }
-
 module.exports = concesionaria;
